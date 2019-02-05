411c0464567513a45a26d95eb3c948912c6d2f41
Nicky Rucker <nickyrucker@hotmail.com>
2019-02-05 11:17:59 -0700

    Update SQL_Exploration.sql

--- SQL_Exploration.sql
+++ SQL_Exploration.sql
@@ -3,3 +3,18 @@ select
   top 10 id, updated, email, first, last, status, name, person.[user]
 from person
 where email = '';
+
+---  All Apps for a person
+select
+  a.person,
+  a.id as APP_ID,
+  a.created,
+  a.updated,
+  a.status,
+  a.bin
+from
+  application a
+where
+  a.person = ''
+
+--
