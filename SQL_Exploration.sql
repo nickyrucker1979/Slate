43806ea944312a2fa101abc7d0dad01c9b76f447
Nicky Rucker <nickyrucker@hotmail.com>
2019-02-05 11:54:57 -0700

    Update SQL_Exploration.sql

--- SQL_Exploration.sql
+++ SQL_Exploration.sql
@@ -17,4 +17,38 @@ from
 where
   a.person = ''
 
---
+--  View Application Responses
+
+select
+  top 100
+  f.record,
+  f.field,
+  --   f.value,
+  isnull(p.value,f.value) response,
+  p.short,
+  p.export,
+  p.category,
+  p.export2,
+  p.export3,
+  p.export4,
+  p.export5,
+  f.timestamp
+
+from
+  field f
+  left join [lookup.prompt] p on f.prompt = p.id
+where
+  f.record in (
+        '',
+        '',
+        '',
+        ''
+  )
+
+order by f.field, f.record
+
+;
+
+
+
+;
