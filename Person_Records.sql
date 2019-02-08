154424e7be63e797945ff0c7c46db4e7a7c37311
Nicky Rucker <nickyrucker@hotmail.com>
2019-02-08 14:17:45 -0700

    Update Person_Records.sql

--- Person_Records.sql
+++ Person_Records.sql
@@ -47,3 +47,45 @@ where
   record = '' -- this is the person id
 
 ;
+
+
+select
+  a.id,
+  a.person,
+  a.created,
+  a.updated,
+  a.status,
+  b.name
+from
+  application a
+  left join [lookup.bin] b
+    on a.bin = b.id
+where
+  a.person = '';
+
+
+  select
+  --   top 100
+    f.record,
+    f.field,
+    --   f.value,
+    isnull(p.value,f.value) response,
+    p.short,
+    p.export,
+    p.category,
+    p.export2,
+    p.export3,
+    p.export4,
+    p.export5,
+    f.timestamp
+  -- *
+  from
+    field f
+    left join [lookup.prompt] p on f.prompt = p.id
+  where
+    f.record in (
+          '' -- app id
+    )
+
+  order by f.field, f.record
+  ;
