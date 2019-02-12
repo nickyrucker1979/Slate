54e54907a4a9b69cea87e1e78547d83acd121a7c
Nicky Rucker <nickyrucker@hotmail.com>
2019-02-12 10:06:14 -0700

    Update Materials.sql

--- Materials.sql
+++ Materials.sql
@@ -1,8 +1,19 @@
+
 select
+  top 100
+  c.record,
+  case when c.section is NULL then lc.section else c.section end as SECTION,
+  case when c.subject is NULL then lc.subject else c.subject end as SECTION,
+  case when c.status is NULL then 'Awaiting' else c.status end as SECTION,
+  c.effective,
+  c.expires
+from
+ [checklist] c
+  left join [lookup.checklist] lc
+    on c.template = lc.id
+where record in
+(
+      '',  -- personid
+      ''  -- app id
 
-  *
-from [checklist]
-where
-  record = '' -- personid  however it doesn't show all the data????
-
-;
+);
