222214d1e37d3c589551043b914667cf3443e257
Nicky Rucker <nickyrucker@hotmail.com>
2019-04-16 15:52:58 -0600

    SlateDataStg Import Scripts

--- Slate_ImportTo_XDW.py
+++ Slate_ImportTo_XDW.py
@@ -1,5 +1,6 @@
 import pyexasol
 from config import config_Exasol as ce
+import os
 
 C = pyexasol.connect(
     dsn=ce.dsn, #connection_string
@@ -7,9 +8,35 @@ C = pyexasol.connect(
     password=ce.password # password
     )
 
-df = C.export_to_pandas("""
-    select * from SLATE_DATA_STG.APPLICATIONS
-    limit 10
- """)
+def executeScriptsFromFile(filename):
+    # Open and read the file as a single buffer
+    fd = open(filename, 'r')
+    sqlFile = fd.read()
+    fd.close()
 
-print(df)
+    # all SQL commands (split on ';')
+    # Final sql statement must not include a semicolon or it will error due to the split
+    sqlCommands = sqlFile.split(';')
+
+    # Execute every command from the input file
+    for command in sqlCommands:
+        # This will skip and report errors
+        try:
+            print(filename + ': ' + command)
+            C.execute(command)
+        except:
+            pass;
+
+
+if __name__ == '__main__':
+
+    # list of SQL files to execute
+    sql_files  = [
+        'xdw_Person_Import.sql',
+        'xdw_Applications_Import.sql',
+        'xdw_Applications_GRAD_Subplans_Import.sql'
+    ]
+
+    # run each sql import file
+    for sql_file in sql_files:
+        executeScriptsFromFile(sql_file)
